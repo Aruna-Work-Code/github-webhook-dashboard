637490a04c1b8e0c6969516f9dfdaeed1c917261
Aruna-Work_Code <parvathamarunasri@gmail.com>
2025-07-11 01:11:00 +0530

    Fix: Bind to 0.0.0.0 and PORT for Render

--- app.py
+++ app.py
@@ -100,5 +100,9 @@ def get_events():
     recent_events = events_collection.find().sort('_id', -1).limit(10)
     return dumps(recent_events)
 
+#if __name__ == '__main__':
+ #   app.run(port=5000)
 if __name__ == '__main__':
-    app.run(port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
